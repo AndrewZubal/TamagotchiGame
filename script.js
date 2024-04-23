42f77b8a80bb6e2dcceddf2fdfe0793db3013326
azubal22 <amzubal@cincinnatistate.edu>
2024-04-23 17:44:35 -0400

    Class created

---
+++ script.js
@@ -0,0 +1,10 @@
+class Tamagotchi {
+    constructor(name) {
+      this.name = name;
+      this.hunger = 5;
+      this.sleepiness = 5;
+      this.boredom = 5;
+      this.age = 0;
+      this.alive = true;
+    }
+}
