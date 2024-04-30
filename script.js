bf2a17f36b28ec12aeb5e40d3e6c784156a4d6cf
azubal22 <amzubal@cincinnatistate.edu>
2024-04-30 13:10:38 -0400

    complete with annotations

--- script.js
+++ script.js
@@ -1,136 +1,83 @@
 class Tamagotchi {
+  // Constructor to initialize Tamagotchi properties
   constructor(name) {
     this.name = "";
-    this.hunger = 5;
-    this.sleepiness = 5;
-    this.boredom = 5;
+    this.hunger = this.sleepiness = this.boredom = 5;
     this.age = 0;
     this.alive = true;
     this.evolved = false;
   }
 
-  feed() {
-    if (this.hunger > 0) {
-      this.hunger--;
-      this.updateMetrics();
-    }
-  }
+  // Methods to perform actions and update metrics
+  feed() { this.hunger > 0 && (this.hunger--, this.updateMetrics()); }
+  turnOffLights() { this.sleepiness > 0 && (this.sleepiness--, this.updateMetrics()); }
+  play() { this.boredom > 0 && (this.boredom--, this.updateMetrics()); }
+  namePet(name) { this.name = name; }
 
-  turnOffLights() {
-    if (this.sleepiness > 0) {
-      this.sleepiness--;
-      this.updateMetrics();
-    }
-  }
-
-  play() {
-    if (this.boredom > 0) {
-      this.boredom--;
-      this.updateMetrics();
-    }
-  }
-
-  namePet(name) {
-    this.name = name;
-  }
-
+  // Method to update UI metrics and check for game over or evolution
   updateMetrics() {
-    document.getElementById('hunger').innerText = this.hunger;
-    document.getElementById('sleepiness').innerText = this.sleepiness;
-    document.getElementById('boredom').innerText = this.boredom;
-    document.getElementById('age').innerText = this.age;
+    ['hunger', 'sleepiness', 'boredom', 'age'].forEach(metric =>
+      document.getElementById(metric).innerText = this[metric]
+    );
 
-    if (this.hunger === 10 || this.sleepiness === 10 || this.boredom === 10) {
+    if ([this.hunger, this.sleepiness, this.boredom].some(metric => metric === 10)) {
       this.alive = false;
       this.gameOver();
-    }
-    else if (this.age === 5 && !this.evolved) {
-      this.evolve();
-    }
+    } else if (this.age === 5 && !this.evolved) this.evolve();
   }
 
-  increaseAge() {
+  // Method to increase hunger, boredom, sleepiness, and age over time
+  increaseMetric(metric, interval) {
     setInterval(() => {
       if (this.alive) {
-        this.age++;
+        this[metric]++;
         this.updateMetrics();
       }
-    }, 20000); 
-  }
-  increaseHunger() {
-    setInterval(() => {
-      if (this.alive) {
-        this.hunger++;
-        this.updateMetrics();
-      }
-    }, 10000); 
-  }
-  increaseBoredom() {
-    setInterval(() => {
-      if (this.alive) {
-        this.boredom++;
-        this.updateMetrics();
-      }
-    }, 10000); 
-  }
-  increaseSleepiness() {
-    setInterval(() => {
-      if (this.alive) {
-        this.sleepiness++;
-        this.updateMetrics();
-      }
-    }, 10000); 
+    }, interval);
   }
 
+  // Method to evolve the Tamagotchi
   evolve() {
     this.evolved = true;
     const petElement = document.getElementById('pet');
     petElement.src = "img/buffdawg.png";
-    petElement.onload = function() {
-      petElement.width = petElement.width / 4;
-      petElement.height = petElement.height / 1.5;
+    petElement.onload = () => {
+      petElement.width /= 4;
+      petElement.height /= 1.5;
     };
   }
 
-  gameOver() {
-    alert(`${this.name} has passed away. Game over!`);
-  }
+  // Method to display game over message
+  gameOver() { alert(`${this.name} has passed away. Game over!`); }
 }
 
 let pet;
 
-function feed() {
-  pet.feed();
-}
+// Event handler functions
+function feed() { pet.feed(); }
+function turnOffLights() { pet.turnOffLights(); }
+function play() { pet.play(); }
 
-function turnOffLights() {
-  pet.turnOffLights();
-}
-
-function play() {
-  pet.play();
-}
-
+// Function to name the pet
 function namePet() {
   const name = document.getElementById('petNameInput').value;
-  const statusHeader = document.getElementById('statusHeader');
-  statusHeader.innerText = `${name}'s Status`;
-
-
-pet.namePet(name);
+  document.getElementById('statusHeader').innerText = `${name}'s Status`;
+  pet.namePet(name);
 }
 
+// Initialize Tamagotchi instance and start metric increase intervals
 window.onload = function() {
   const name = "";
   pet = new Tamagotchi(name);
-  pet.increaseAge();
-  pet.increaseHunger();
-  pet.increaseBoredom();
-  pet.increaseSleepiness();
-  petElement.width = petElement.width / 8;
-  petElement.height = petElement.height / 8;
+  ['increaseAge', 'increaseHunger', 'increaseBoredom', 'increaseSleepiness'].forEach(method =>
+    pet[method]()
+  );
+  const petElement = document.getElementById('pet');
+  petElement.width /= 8;
+  petElement.height /= 8;
 };
 
+// Function to make pet move back and forth on the screen
 let direction = 1;
 setInterval(() => {
   const petElement = document.getElementById('pet');
